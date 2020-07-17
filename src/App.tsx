060a8e1a782d8c01a4df246b9f08b35f0506370e
Luan Rohde <luanrohde11@gmail.com>
2020-07-17 06:38:40 -0300

    chore: installation and add status bar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StatusBar} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <View style={{flex: 1, backgroundColor: '#312e38'}} />
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    </>
+  );
+};
+
+export default App;
