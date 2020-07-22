15ad42a5acdf8bee7aae7460a70790948025be26
Luan Rohde <luanrohde11@gmail.com>
2020-07-22 07:18:35 -0300

    feat: signin screen

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+  margin-top: 8px;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
